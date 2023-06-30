1cc0b3082af11a011aface2315997a2f19dd421f
Jasper <jasper1024028840@gmail.com>
2023-06-30 11:26:42 +0800

    ci：集成pinia、normalize.css、vue-router、less已经环境路径别名配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// 处理路径需要使用绝对路径
+const _resolve = dir => path.resolve(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': _resolve('src'),
+      components: _resolve('src/components')
+    }
+  }
 })
